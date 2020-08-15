cd1354b3ff426bb45dfb71483b89e9f068e7eaf9
vedantpople <vedantpople4@gmail.com>
2020-08-15 14:29:32 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Aman Dhatterwal',
+        'title':'Blog on Flutter',
+        'content':'First blog post on flutter',
+        'date_posted':'August 15, 2020'
+    },
+    {
+        'author': 'Love Babbar',
+        'title':'Blog on Machine Learning',
+        'content':'First blog post on ML',
+        'date_posted':'August 16, 2020'
+    }
+]
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html',posts=posts)
+
+@app.route("/about")
+def hello():
+    return render_template('about.html', title = 'About')
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
