7d3d994a925cbb41c7f238154fde9566fe838389
vedantpople <vedantpople4@gmail.com>
2020-08-18 16:13:32 +0530

    New

--- app.py
+++ app.py
@@ -36,9 +36,16 @@ def register():
         return redirect(url_for('home'))
     return render_template('register.html',title='Register', form=form)
 
-@app.route("/login")
+@app.route("/login", methods=['GET','POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!','success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsucessful. Please check Username and password','danger')
+
     return render_template('login.html',title='=Login', form=form)
 
 
