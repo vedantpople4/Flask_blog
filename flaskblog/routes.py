7358b6fde180ec7cf47ba268b99d7f6d915915eb
vedantpople <vedantpople4@gmail.com>
2020-09-22 23:23:42 +0530

    New page pagination

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -11,7 +11,8 @@ from PIL import Image
 @app.route("/")
 @app.route("/home")
 def home():
-    posts = Post.query.all()
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.paginate(page = page, per_page=5)
     return render_template('home.html', posts=posts)
 
 @app.route("/about")
