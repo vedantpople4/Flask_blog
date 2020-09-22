e8a14c2e23342115e003baeecdbe40f503e4bf38
vedantpople <vedantpople4@gmail.com>
2020-09-22 23:45:39 +0530

    user_post route

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -12,7 +12,7 @@ from PIL import Image
 @app.route("/home")
 def home():
     page = request.args.get('page', 1, type=int)
-    posts = Post.query.paginate(page = page, per_page=5)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page = page, per_page=5)
     return render_template('home.html', posts=posts)
 
 @app.route("/about")
@@ -135,3 +135,11 @@ def deletepost(post_id):
     flash('Your post is deleted!','Sucess')
     return redirect(url_for('home'))
 
+@app.route("/user/<string:username>")
+def user_posts(username):
+    page = request.args.get('page', 1, type=int)
+    user = User.query.filter_by(username= username).first_or_404()
+    posts = Post.query.filter_by(author=user)\
+        .order_by(Post.date_posted.desc())\
+        .paginate(page = page, per_page=5)
+    return render_template('user_post.html', posts=posts, user=user)
