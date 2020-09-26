4d8f71cb8e7fdb94a546ce4ce8dcfaa4944abd6c
vedantpople <vedantpople4@gmail.com>
2020-09-26 16:14:05 +0530

    importing Mail package from init

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -19,5 +19,6 @@ app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
 app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+mail = Mail(app)
 
 from flaskblog import routes
--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,8 +1,9 @@
 from flask import render_template, url_for, flash, redirect, request, abort
-from flaskblog import app, db, bcrypt
+from flaskblog import app, db, bcrypt, mail
 from flaskblog.models import User,Post
 from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm, RequestResetForm, ResetPasswordForm
 from flask_login import login_user, current_user, logout_user, login_required
+from flask_mail import Message
 import secrets
 import os
 from PIL import Image
