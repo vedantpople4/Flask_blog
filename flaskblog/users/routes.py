48fa57889823414665e9dec23d3e3ce8ccb4ddbe
vedantpople <vedantpople4@gmail.com>
2020-09-28 23:47:14 +0530

    Commit

--- flaskblog/users/routes.py
+++ flaskblog/users/routes.py
@@ -4,16 +4,8 @@ from flaskblog import db, bcrypt
 from flaskblog.models import User, Post
 from flaskblog.users.forms import RegistrationForm, LoginForm, UpdateAccountForm, RequestResetForm, ResetPasswordForm
 from flaskblog.users.utils import save_picture, send_reset_email
-
-
-
-
 from flask import Blueprint
-
 users = Blueprint('users',__name__)
-
-
-
 
 @users.route("/register", methods=['GET','POST'])
 def register():
